f23abfa65fd74c1bb3e8cb0fa7590b2fe51276a5
eliesium <elieone00@gmail.com>
2018-06-05 19:49:27 +0800

    Created adding of new candidates with 2 atributes

--- app/components/candidates/candidatescontroller.js
+++ app/components/candidates/candidatescontroller.js
@@ -1,13 +1,15 @@
 angular.module('MyAgileHr').controller('CandidatesController', function($scope) {
-  $scope.message = 'Hello from CandidatesController';
-  $scope.candidatesList = ["Milet", "Jay", "Betty"];
+  $scope.pageHeader = 'Candidates';
+  $scope.pageHeaderDetails = 'This is where you can manage all the applicants. You can add an new applicant data, including their exam date, time, scores and attach their resumes. You can also tag the profiles to open requisitions where they seem to fit. These line manages will be notified via email, together with the exam scores and resume so they can check themselves if HR can proceed to schedule the candidates for interview.';
+  $scope.candidatesList = [{name: "Milet", age:15}, {name: "Jay", age:27}, {name: "Betty", age:40}];
   $scope.addItem = function () {
       $scope.errortext = "";
-      if (!$scope.newCandidate) { return; }
-      if ($scope.candidatesList.indexOf($scope.newCandidate) == -1) {
-          $scope.candidatesList.push($scope.newCandidate);
-      } else {
+      if (!$scope.newCandidateName) { return; }
+      var searchResults = $scope.candidatesList.filter(function(searchableCandidate){ return searchableCandidate.name === $scope.newCandidateName });
+      if(searchResults.length > 0) {
           $scope.errortext = "The item is already in our candidate list.";
+      } else {
+        $scope.candidatesList.push({name: $scope.newCandidateName, age: $scope.newCandidateAge});
       }
   }
   $scope.removeItem = function (x) {
