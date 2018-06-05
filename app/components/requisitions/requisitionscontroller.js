91f0086943839ee911290d51565e48629ffe482c
eliesium <elieone00@gmail.com>
2018-06-05 22:18:16 +0800

    Agile HR better styles

--- app/components/requisitions/requisitionscontroller.js
+++ app/components/requisitions/requisitionscontroller.js
@@ -1,3 +1,4 @@
 angular.module('MyAgileHr').controller('RequisitionsController', function($scope) {
-  $scope.message = 'Hello from RequisitionsController';
+  $scope.pageHeader = 'Requisitions';
+  $scope.pageHeaderDetails = 'This is where you can manage all the requisitions. You can open new requisition requests, and specify recommended job grades, needed skills and certifications, minimum years of experience. Candidates who are a possible fit to these requisitions will also be mapped. This is where line managers can select from the candidates tagged to them and notify HR to schedule the candidate for interview.';
 });
--- app/components/scheduler/schedulercontroller.js
+++ app/components/scheduler/schedulercontroller.js
@@ -1,3 +1,4 @@
 angular.module('MyAgileHr').controller('SchedulerController', function($scope) {
-  $scope.message = 'Hello from SchedulerController';
+  $scope.pageHeader = 'Scheduler';
+  $scope.pageHeaderDetails = 'This is where the HR can track when candidates are scheduled for interview. It also has a task list for candidates who need to be followed up. Maintenance for the levels of interview are also found here. By default, yellow tags are for technical interview and light orange are for behavioral interview. Green tags are for job offer schedules. If after 1 day, a candidate does not advance to any of these levels, a task to follow up will appear on the task list.';
 });
